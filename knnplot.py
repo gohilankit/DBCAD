379c90aedc25daec09b296e99bd734ec59efb61b
anishgt <anishgt.1989@gmail.com>
2016-04-12 16:03:34 -0400

    knn plot

---
+++ knnplot.py
@@ -0,0 +1,54 @@
+from netCDF4 import Dataset
+import numpy as np
+from fastdtw import fastdtw
+from scipy.spatial.distance import euclidean
+import matplotlib.pyplot as plt
+
+
+neighbourlong=[0,1,1,1,0,-1,-1,-1]
+neighbourlat =[-1,-1,0,1,1,1,0,-1]
+globalcorepoints=set()
+globalnoisepoints=set(range(480))
+globalborderpoints=set()
+dist=[]
+Modedict={}
+f = Dataset("ALDA_dataset_and_initial_code/air.2m.gauss.1979.nc", "r+", format="NETCDF4")
+def testFunc():
+  timewindow=9
+  iter=0
+  for timeIndex in range (4,5,1):
+    for iterlong in range(126,158):
+      for iterlat in range(20,35):
+        currtimeslice = f.variables['air'][timeIndex - timewindow/2  : timeIndex + timewindow/2 + 1, 0, iterlat, iterlong]
+        for iter in range(8):
+           neighbourtimeslice = f.variables['air'][timeIndex - timewindow/2  : timeIndex + timewindow/2 + 1, 0, iterlat +neighbourlat[iter], iterlong+neighbourlong[iter]]
+           distance, path = fastdtw(currtimeslice, neighbourtimeslice, dist=euclidean)
+           #print distance
+           dist.append((distance,iter))
+           iter+=1
+
+
+def main():
+  testFunc()
+  print dist
+  fig=plt.figure()
+  ax=fig.add_subplot(111)
+  #y,x,_=plt.hist(dist,bins=100)
+  #for i in range(0,len(y)):
+      #if y[i]== y.max():
+          #print x[i]
+          #break
+#
+  #fig.savefig("plot.png")
+  #nmax=np.max(n)
+  #arg_max=None
+  #for j,_n in enumerate(n):
+      #if _n==max:
+          #arg_max=j
+          #break
+
+  print dist[arg_max]
+
+
+if __name__ == '__main__':
+  main()
